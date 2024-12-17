be9b41a97cc6a8ebf61917a773d310076a679567
Francisco Fernandez <fraanfeernaandeez@gmail.com>
2024-12-17 09:27:18 +0100

    Conditionals exercise finished, let's continue!

--- src/main/kotlin/functions/functions.kt
+++ src/main/kotlin/functions/functions.kt
@@ -1,7 +1,11 @@
 package functions
 
+import java.util.*
+
 /*
 @author: Francisco Fernandez Navarro
 #date: 17 de Diciembre de 2024
 %file: functions.kt\
 */
+
+val SCANNER: Scanner = Scanner(System.`in`)
